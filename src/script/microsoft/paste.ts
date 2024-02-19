2ea7367d855531550266ec537e49832031f9c1af
xi4f3i <xi4f3i@gmail.com>
2024-02-19 08:34:11 +0000

    feat(script): add windows paste script

--- src/script/microsoft/paste.ts
+++ src/script/microsoft/paste.ts
@@ -1,3 +1,12 @@
-export const pasteForWindows = ``;
+import { execSync } from 'child_process';
 
-export const winscript = async () => {};
+export const pasteForWindows = `$wshell = New-Object -ComObject wscript.shell;
+$wshell.SendKeys("^(c)")`;
+
+export const winscript = async () => {
+  try {
+    execSync(pasteForWindows, { shell: 'powershell.exe' });
+  } catch (err) {
+    console.error(err);
+  }
+};
