15b0d992cd20b27c6a2b5c6adea7be3990552463
YuanG1944 <371115609@qq.com>
2025-03-19 23:19:09 +0800

    change config ui

--- src/views/ClipHistoryBoard/index.tsx
+++ src/views/ClipHistoryBoard/index.tsx
@@ -323,7 +323,7 @@ const ClipHistoryBoard: FC = () => {
     setQueryKey(value);
   };
 
-  const handleContextMenu = (evt: MouseEvent) => {
+  const handleContextMenu = (evt: any) => {
     !isDev() && evt.preventDefault();
   };
 
